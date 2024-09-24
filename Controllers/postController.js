328a0749198bf5fd5ae5ea04878423585d97d7f8
Amirali Salari <amirali.salar@outlook.com>
2024-09-24 20:20:49 +0330

    Data validation was added to the controller.

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -1,6 +1,28 @@
-import Blog from '../Models/Blog.js'
+import Blog from "../Models/Blog.js";
+import mongoose from "mongoose";
 
 export const createPost = async (req, res) => {
-    const article = await Blog.create(req.body)
-    console.log(article)
-}
+  try {
+    const article = await Blog.create(req.body);
+
+    return res.status(201).json(article);
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      let errors = {};
+
+      Object.keys(error.errors).forEach((key) => {
+        errors[key] = error.errors[key].message;
+      });
+
+      return res.status(400).json({
+        message: "ValidationError",
+        errors: errors,
+      });
+    } else {
+      return res.status(400).json({
+        message: "UnexpectedError",
+        errors: error,
+      });
+    }
+  }
+};
--- Models/Blog.js
+++ Models/Blog.js
@@ -1,24 +1,27 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
-const blogSchema = new Schema({
+const blogSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: [true, "Title is a required field."],
     },
     content: {
-        type: String,
-        required: true
+      type: String,
+      required: [true, "Content is a required field."],
     },
     category: {
-        type: String,
-        required: true
+      type: String,
+      required: [true, "Category is a required field."],
     },
     tags: {
-        type: [String],
-        required: false
+      type: [String],
+      required: false,
     },
-})
+  },
+  { timestamps: true }
+);
 
-const Blog = model('Blog', blogSchema);
+const Blog = model("Blog", blogSchema);
 export default Blog;
